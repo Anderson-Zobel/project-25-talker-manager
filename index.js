94d1cbebb3a92e2f8988c7c3bdade68cab230caf
Anderson Zobel <87586643+Anderson-Zobel@users.noreply.github.com>
2022-02-22 17:11:03 -0300

    unnecessary comments

--- index.js
+++ index.js
@@ -70,7 +70,6 @@ app.delete(
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
